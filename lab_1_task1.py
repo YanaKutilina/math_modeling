9bbf92f0b6663835b5b83479db98f88789f4e22a
YanaKutilina <98036210+YanaKutilina@users.noreply.github.com>
2022-02-02 13:36:38 +0000

    дз

--- lab_1_task1.py
+++ lab_1_task1.py
@@ -1,7 +1,22 @@
 print("Hello, World!")
+
 print("Маша + Петя = Любовь")
+
 a=3
 b=4
 print("x","=",a,'+',b)
+
 x = 3 + 4
 print("x =", x)
+
+x= "Hello, World!"
+print(type(x))
+
+x=3+4
+print(type(x))
+
+x=3/4
+print(type(x))
+
+x=[1,2,5,10,100]
+print(type(x))
