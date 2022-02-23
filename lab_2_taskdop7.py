2fdacbf85ec7e110991ca3fd68ad74071516e7f3
YanaKutilina <98036210+YanaKutilina@users.noreply.github.com>
2022-02-23 15:01:00 +0000

    домашняя работа все,кроме2

--- lab_2_taskdop7.py
+++ lab_2_taskdop7.py
@@ -1,10 +1,4 @@
 for i in range(1,10):
-  print(i,end=' ')
-  for f in range(1,10):
-    print(i+f,end=' ')
-print()
-for i in range(2,10):
-  print(i)
-
-
-
+  for j in range(i, i*10, i):
+         print(j, end='\t')
+     print()
