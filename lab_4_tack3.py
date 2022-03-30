b1b4b957ea52e070799bbebb7aa1ca85b1ba99c0
YanaKutilina <98036210+YanaKutilina@users.noreply.github.com>
2022-03-30 15:10:17 +0000

    lp

---
+++ lab_4_tack3.py
@@ -0,0 +1,16 @@
+Grav_const =6.67 *10**(-11)
+light_speed= 3 * 108 
+g= 9.80665 
+Pi=3.14159265359
+Avagadro_number=6.022140857 * (10**23)
+Bocman=1.38*10**(-23)
+Quantum_conductivity= 96485.33289
+Neutron_mass= 1.674927471*(10**-27)
+Electron_mass=9.10938356*(10**(-31))
+Proton_mass=1.672621898*(10**(-27))
+hight = 6.62*10**(-34)
+eller=2.71828
+
+def mex_end(a,b,c):
+  print((a*c**2/2)+(a*g*b))
+mex_end(a=10,b=5,c=6)
